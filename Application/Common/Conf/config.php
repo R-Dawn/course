38f85e03b7412dd03ed545fa18d841aca011aa2d
Dawn Rao <857756968@qq.com>
2015-01-13 20:45:07 +0800

    更改数据库连接地址

--- Application/Common/Conf/config.php
+++ Application/Common/Conf/config.php
@@ -1,11 +1,11 @@
 ﻿<?php
 return array(
 	'DB_TYPE'   => 'mysql', // 数据库类型
-    'DB_HOST'   => 'localhost', // 服务器地址
+    'DB_HOST'   => 'dawnmj.xicp.net', // 服务器地址
     'DB_NAME'   => 'hm', // 数据库名
-    'DB_USER'   => 'root', // 用户名
-    'DB_PWD'    => '', // 密码
-    'DB_PORT'   => 3306, // 端口    
+    'DB_USER'   => 'ci', // 用户名
+    'DB_PWD'    => 'iamdawn', // 密码
+    'DB_PORT'   => 16490, // 端口    
     'DB_CHARSET'=> 'utf8', // 字符集
 
      'host'=>'smtp.qq.com',
